ff90dc25d3908017d21648f98bce8634bf90dbac
Felipe Morilho <felipemdecastro@gmail.com>
2023-04-14 18:10:55 -0300

    Adicionando os Idiomas dinamicamente

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,10 +33,18 @@ function updateHardSkills(profileData){
 
         hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
 }
+
+function updateLanguages(profileData){
+        const languages = document.getElementById('profile.languages')
+
+        hardSkills.innerHTML = profileData.skills.hardSkills.map(language => `<li>${language}</li>`).join('')
+}
+
 (async () => {
         const profileData = await fetchProfileData()
         updateProfileInfo(profileData)
         updateSoftSkills(profileData)
         updateHardSkills(profileData)
+        updateLanguages(profileData)
 })()
 
