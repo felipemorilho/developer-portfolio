dd0e43d2394d028f6bcf0f964e0d6b29cfbf3973
Felipe Morilho <felipemdecastro@gmail.com>
2023-04-14 07:32:21 -0300

    Alterando o json e adicionando softSkills

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,12 +19,18 @@ function updateProfileInfo(profileData){
 
         const email = document.getElementById('profile.email')
         email.innerText = profileData.email
-        email.href = `mailto: ${profileData.email}`
+        email.href = `mailto:${profileData.email}`
 }
 
+function updateSoftSkills(profileData){
+        const softSkills = document.getElementById('profile.skills.softSkills')
+
+        softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
 
 (async () => {
         const profileData = await fetchProfileData()
-        console.log(profileData)
+        updateProfileInfo(profileData)
+        updateSoftSkills(profileData)
 })()
 
