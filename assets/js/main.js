0dadbe27193e486b4e578ecf24ab5a28855dfcab
Felipe Morilho <felipemdecastro@gmail.com>
2023-04-14 18:03:11 -0300

    Adicionando as HardSSkills e acertando o json.

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,13 +29,14 @@ function updateSoftSkills(profileData){
 }
 
 function updateHardSkills(profileData){
-        const hardSkills = document.getElementById('')
+        const hardSkills = document.getElementById('profile.skills.hardSkills')
 
-        hardSkills.innerHTML = 
+        hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
 }
 (async () => {
         const profileData = await fetchProfileData()
         updateProfileInfo(profileData)
         updateSoftSkills(profileData)
+        updateHardSkills(profileData)
 })()
 
